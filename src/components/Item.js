81c09907e928d1634cd54e0f9fd714cdb479937e
Nathan Welliver <nwelliver.564@gmail.com>
2024-04-29 15:58:53 -0400

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, itemInCart] = useState(false)
+
+  const prdctInCart = inCart ? "in-cart" : "";
+
+  function handleClick(){
+    itemInCart((inCart) => !inCart)
+  }
+
   return (
-    <li className="">
+    <li className={prdctInCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
